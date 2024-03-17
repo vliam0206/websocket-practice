f778d8c834f595960188ee5b039b39de811a0da7
viliam0206 <v.trclam@gmail.com>
2024-03-17 16:28:54 +0700

    Add WebSocket api

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 ## Demo
 <img src="./demo.gif" align="center"/>
+
+## References
+![WebSocket client in C#](https://youtu.be/TjfFPMCPBVg?si=z6ve8-1ynSjBBHll)
+![WebSocket server in C#](https://youtu.be/ThiAQAB5Dp4?si=sOq0rjSY3ZNWLI0W)
+![WebSocket API in C#](https://youtu.be/hNoCIW4iM10?si=KWr6BQVrgP6vdnCJ&t=10312)
---
+++ websocket-api/websocket-server-api/Controllers/WebSocketDemosController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace websocket_server_api.Controllers;
+
+[Route("api/ws")]
+[ApiController]
+public class WebSocketDemosController : ControllerBase
+{
+    [HttpGet]
+    public async Task Get()
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using (var ws = await HttpContext.WebSockets.AcceptWebSocketAsync())
+            {
+                // generate values x,y random every 2 seconds
+                var random = new Random();
+                while (ws.State == WebSocketState.Open)
+                {
+                    int x = random.Next(1, 100);
+                    int y = random.Next(1, 100);
+                    var stringValue = $"{{ \"x\": {x}, \"y\": {y} }}";
+                    var buffer = Encoding.UTF8.GetBytes(stringValue);
+                    await ws.SendAsync(
+                        new ArraySegment<byte>(buffer),
+                        WebSocketMessageType.Text, true, CancellationToken.None);
+                    // waiting for 2 seconds for the next pair of values
+                    await Task.Delay(2000);                    
+                }
+                // close ws connection
+                await ws.CloseAsync(WebSocketCloseStatus.NormalClosure,
+                    "WebSocket connection closed by Server.", CancellationToken.None);
+            }
+        } else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status406NotAcceptable;
+        }
+    }
+}
